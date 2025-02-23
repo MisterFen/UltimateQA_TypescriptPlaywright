5414da78e741a5b70f0cb9196febcd0b4e4492ae
MisterFen <40703597+MisterFen@users.noreply.github.com>
2025-02-23 20:34:11 +0000

    Catch screenshots on failure

--- tests/filling_out_forms.spec.ts
+++ tests/filling_out_forms.spec.ts
@@ -1,14 +1,16 @@
 import { test, expect } from "@playwright/test";
 import { FillingOutFormsPage } from "../pages/filling_out_forms_page"
 
-test("should submit the contact form and show a success message", async ({ page }) => {
-    const contactForm = new FillingOutFormsPage(page);
+test.describe("Filling out forms", () => {
+    test("should submit the contact form and show a success message", async ({ page }) => {
+        const contactForm = new FillingOutFormsPage(page);
+        
+        await contactForm.navigate();
+        await contactForm.fillForm("John Doe", "Hello!");
+        await contactForm.submitForm();
 
-    await contactForm.navigate();
-    await contactForm.fillForm("John Doe", "Hello!");
-    await contactForm.submitForm();
-
-    await contactForm.getSuccessMessage().waitFor({ state: "visible", timeout: 5000 });
-    const message = await contactForm.getSuccessMessage().textContent();
-    console.log("Actual message:", message);
+        await contactForm.getSuccessMessage().waitFor({ state: "visible", timeout: 5000 });
+        const message = await contactForm.getSuccessMessage().textContent();
+        console.log("Actual message:", message);
+    });
 });
