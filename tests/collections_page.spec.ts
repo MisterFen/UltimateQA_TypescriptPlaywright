aae57ebc7a3ea76d1c6166c62a59999314c1c000
MisterFen <40703597+MisterFen@users.noreply.github.com>
2025-02-23 23:24:42 +0000

    Final removal of auth
    Confirmed that this project is not suited for testing auth until reliable Cloudflare workaround is achieved. In CI, it triggers too frequently around login

--- tests/collections_page.spec.ts
+++ tests/collections_page.spec.ts
@@ -13,12 +13,12 @@ test.describe('Cloudflare Protected Tests', () => {
         await expect(collectionsPage.isUserLoggedIn()).resolves.toBeFalsy();
     });
 
-    test.describe('Authenticated User Tests', () => {
-        test.use({ storageState: path.join(__dirname, '../playwright/.auth/user.json')});
+    // test.describe('Authenticated User Tests', () => {
+    //     test.use({ storageState: path.join(__dirname, '../playwright/.auth/user.json')});
     
-        test('User should be logged in', async ({ collectionsPage }) => {
-            await collectionsPage.goto();
-            await expect(collectionsPage.isUserLoggedIn()).resolves.toBeTruthy();
-        });
-    });
+    //     test('User should be logged in', async ({ collectionsPage }) => {
+    //         await collectionsPage.goto();
+    //         await expect(collectionsPage.isUserLoggedIn()).resolves.toBeTruthy();
+    //     });
+    // });
 });
