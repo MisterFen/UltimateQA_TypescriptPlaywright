6b45612d57da3df280952a7405ade41dbbe206a0
MisterFen <40703597+MisterFen@users.noreply.github.com>
2025-02-23 20:43:43 +0000

    Labelled cloudflare protected pages, so they don't run in CI

--- tests/collections_page.spec.ts
+++ tests/collections_page.spec.ts
@@ -1,11 +1,17 @@
 import { test, expect } from '../fixtures/login_fixtures';
 
+test.describe('Cloudflare Protected Tests', () => {
+    test.beforeEach(({ page }) => {
+      if (process.env.CI) {
+        test.skip(true, 'Skipped in CI due to Cloudflare issues.');
+      }
+    });
 
-test.describe('UnAuthenticated User Tests', () => {
     test('User should not be logged in', async ({ collectionsPage }) => {
         await collectionsPage.goto();
         await expect(collectionsPage.isUserLoggedIn()).resolves.toBeFalsy();
     });
+
 });
 
 // test.describe('Authenticated User Tests', () => {
